1835b55102f1029b673a76608250a52d53468fab
chrisG759 <150283599+chrisG759@users.noreply.github.com>
2025-02-13 09:33:01 -0500

    Set node env for production

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(session({
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    cookie: { 
+        secure: process.env.NODE_ENV === 'production', 
+        httpOnly: true 
+    }
 }));
 
 app.set('view engine', 'ejs');
